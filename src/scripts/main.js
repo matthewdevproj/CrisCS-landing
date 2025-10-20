c97ca74dfda179fe663794e68a1d237eb77eeb37
yamhilpusaico <pusaicocordovay@gmail.com>
2025-10-20 10:20:15 -0500

    feature: about, contact form fixes, carousel fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,5 +2,5 @@ import 'flowbite';
 
 document.addEventListener('astro:page-load', () => {
   // Re-inicializa Flowbite en cada cambio de ruta
-    if (window.initFlowbite) window.initFlowbite();
+  if (window.initFlowbite) window.initFlowbite();
 });
